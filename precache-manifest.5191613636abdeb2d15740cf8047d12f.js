31c06b357d94ebdf00570188ac94df78263688ff
Mohammed Akhlad Ubada <142223228+Mohammed-Akhlad-Ubada@users.noreply.github.com>
2024-03-03 20:53:55 +0530

    Updates

---
+++ precache-manifest.5191613636abdeb2d15740cf8047d12f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e97bde5c108ea6a7abbe170f4e42ae8",
+    "url": "/TextUtil/index.html"
+  },
+  {
+    "revision": "1bddec1d1b3f2c35c5b1",
+    "url": "/TextUtil/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "9702f6d54385a573b86a",
+    "url": "/TextUtil/static/js/2.8f01fc04.chunk.js"
+  },
+  {
+    "revision": "184544feb51a777f05eb9d4b59832005",
+    "url": "/TextUtil/static/js/2.8f01fc04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a305264669ab0698eb9",
+    "url": "/TextUtil/static/js/3.a8bd2e92.chunk.js"
+  },
+  {
+    "revision": "1bddec1d1b3f2c35c5b1",
+    "url": "/TextUtil/static/js/main.5744a7ce.chunk.js"
+  },
+  {
+    "revision": "379b587571aa7c7f9592",
+    "url": "/TextUtil/static/js/runtime-main.9f3a0ff1.js"
+  }
+]);
